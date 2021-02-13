a8088760e59ab8731ccb952956f566795b56402c
Pawan <47448422+pawansingh10@users.noreply.github.com>
2021-02-13 21:28:08 +0530

    Add files via upload

---
+++ personalGoogleSearch.py
@@ -0,0 +1,21 @@
+# Python Script for personal googler searcher
+# Code with fun
+
+import requests
+import bs4
+import sys
+import webbrowser
+
+res = requests.get('https://google.com/search?q='+''.join(sys.argv[1:]))
+
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text,"html.parser")
+
+linkElements = soup.select('.r a')
+
+linkToOpen = min(5,len(linkElements))
+
+for i in range(linkToOpen):
+    webbrowser.open("https://google.com"+linkElements[i].get('href'))
+
